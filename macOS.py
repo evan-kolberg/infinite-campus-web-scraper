cf153fecfb7cb8aba528320b104a831623a05c6f
Evan Kolberg <ekprogamer13@gmail.com>
2022-03-18 16:24:51 -0400

    cleaned up

--- macOS.py
+++ macOS.py
@@ -19,24 +19,20 @@ driver.set_window_position(1200, 200, windowHandle='current')
 def login():
     driver.get('https://campus.bellmore-merrick.k12.ny.us/campus/portal/students/bellmore.jsp')
     WebDriverWait(driver, 8).until(
-        expected_conditions.presence_of_element_located((By.ID, 'username'))
-    )
-    WebDriverWait(driver, 8).until(
-        expected_conditions.presence_of_element_located((By.ID, 'password'))
-    )
+        expected_conditions.presence_of_element_located((By.ID, 'username'))).send_keys(input('Student ID:  '))
 
-    driver.find_element(By.NAME, 'username').send_keys(input('Student ID:  '))
-    driver.find_element(By.NAME, 'password').send_keys(input('Password:  '))
-    driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/form/input[6]').click()
+    WebDriverWait(driver, 8).until(
+        expected_conditions.presence_of_element_located((By.ID, 'password'))).send_keys(input('Password:  '))
+
+    WebDriverWait(driver, 8).until(
+        expected_conditions.presence_of_element_located(
+            (By.XPATH, '/html/body/div/div[2]/div[1]/form/input[6]'))).click()
 
 
 def grades_page():
     WebDriverWait(driver, 8).until(
         expected_conditions.presence_of_element_located(
-            (By.XPATH, '/html/body/ic-nav-wrapper-app/ic-sidebar/div/ic-tool-list/nav/ul/li[4]/a'))
-    )
-
-    driver.find_element(By.XPATH, '/html/body/ic-nav-wrapper-app/ic-sidebar/div/ic-tool-list/nav/ul/li[4]/a').click()
+            (By.XPATH, '/html/body/ic-nav-wrapper-app/ic-sidebar/div/ic-tool-list/nav/ul/li[4]/a'))).click()
 
 
 def get_those_grade_updates():
@@ -52,3 +48,4 @@ if __name__ == '__main__':
     grades_page()
     get_those_grade_updates()
     driver.close()
+
